21e6ea1dd4357971000f83c5bafd99dbb5f91d84
vinhuyen <vinhuyen.theonly@gmail.com>
2022-07-14 11:56:19 +0700

    jobs

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import Proptypes from 'prop-types';
-import '../asset/scss/JobsStyle.scss'
+import '../asset/scss/JobsStyle.scss';
+
+export default function JobCard(props) {
+    return(
+        <div>
+            <div className="job--card--left">
+                <div className="job__card__location"></div>
+                <div className="job__title"></div>
+                <div className="job__salary"></div>
+                <div className="job__position__type"></div>
+                <div className="job__company"></div>
+                <button></button>
+            </div>
+            <div className="job__card_right">
+                <div className="job__title__child"></div>
+            </div>
+        </div>
+    )
+}
