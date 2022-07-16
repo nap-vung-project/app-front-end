107ca7ec2a0c47c684faeb1102eaed1fdaac87c7
luyenngongo2002 <luyenngongo3112@gmail.com>
2022-07-16 08:32:25 +0700

    Update

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DetaiJob from './components/detail-job/detaiJob';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <DetaiJob />
   </React.StrictMode>
 );
 
