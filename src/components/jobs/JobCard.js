b71373d87bc22b97bc1d78e4e88843883f4ace97
vinhuyen <vinhuyen.theonly@gmail.com>
2022-07-16 09:32:19 +0700

    uyen-jobs

--- src/components/jobs/JobCard.js
+++ src/components/jobs/JobCard.js
@@ -6,33 +6,33 @@ import '../../asset/scss/JobsStyle.scss'
 
 export default function JobCard(props) {
     return(
-        <div>
-            <div className="job--card--left">
-                <div className="job__card__location">
+        <div className="job-card">
+            <div className="job-card-left">
+                <div className="job-card-location">
                     <GrLocation value={{color: 'red', size: '15px'}}/>
                     <span>Da Nang</span>
                 </div>
-                <div className="job__title">Software Engineer</div>
-                <div className="job__salary">
+                <div className="job-card-title">Software Engineer</div>
+                <div className="job-card-salary">
                     <span>Salary</span>
                     <span>$1500</span>
                 </div>
-                <div className="job__position__type">
+                <div className="job-card-position-type">
                     <span>Position type</span>
                     <span>Full time</span>
                 </div>
-                <div className="job__company">
+                <div className="job-card-company">
                     <span>Company</span>
                     <span>O2R2</span>
                 </div>
                 <button></button>
             </div>
-            <div className="job__card_right">
-                <div className="job__title__child"></div>
-                <div className="job__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cupiditate fugit labore numquam veritatis! Ab asperiores aut commodi dicta eius ipsa maxime, numquam, obcaecati quam rem sed velit veritatis!</div>
-                <div className="job__description"></div>
-                <div className="job__createAt"></div>
-                <a className="read__more"></a>
+            <div className="job-card-right">
+                <div className="job-card-title-child"></div>
+                <div className="job-card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cupiditate fugit labore numquam veritatis! Ab asperiores aut commodi dicta eius ipsa maxime, numquam, obcaecati quam rem sed velit veritatis!</div>
+                <div className="job-card-description"></div>
+                <div className="job-card-createAt"></div>
+                <a className="read-more"></a>
             </div>
         </div>
     )
